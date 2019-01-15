ed62ac453a2174778a2e3bf8f3b45da485fa2b1f
Nathan <dongdly@gmail.com>
2019-01-15 16:31:49 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,126 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    inputShowed: false,
+    inputVal: ""
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function() {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+
+  showInput: function() {
+    this.setData({
+      inputShowed: true
+    });
+  },
+  hideInput: function() {
+    this.setData({
+      inputVal: "",
+      inputShowed: false
+    });
+  },
+  clearInput: function() {
+    this.setData({
+      inputVal: ""
+    });
+  },
+  inputTyping: function(e) {
+    this.setData({
+      inputVal: e.detail.value
+    });
+  },
+  takePhoto: function() {
+    const ctx = wx.createCameraContext()
+    ctx.takePhoto({
+      quality: 'high',
+      success: (res) => {
+        this.setData({
+          src: res.tempImagePath
+        })
+      }
+    })
+  },
+  getLocalImage: function(){
+    var that = this;
+    wx.chooseImage({
+      count: 1,
+      success: function (res) {
+        // 这里无论用户是从相册选择还是直接用相机拍摄，拍摄完成后的图片临时路径都会传递进来
+        app.startOperating("保存中")
+        var filePath = res.tempFilePaths[0];
+        var session_key = wx.getStorageSync('session_key');
+        // 这里顺道展示一下如何将上传上来的文件返回给后端，就是调用wx.uploadFile函数
+        wx.uploadFile({
+          url: app.globalData.url + '/home/upload/uploadFile/session_key/' + session_key,
+          filePath: filePath,
+          name: 'file',
+          success: function (res) {
+            app.stopOperating();
+            // 下面的处理其实是跟我自己的业务逻辑有关
+            var data = JSON.parse(res.data);
+            if (parseInt(data.status) === 1) {
+              app.showSuccess('文件保存成功');
+            } else {
+              app.showError("文件保存失败");
+            }
+          }
+        })
+      },
+      fail: function (error) {
+        console.error("调用本地相册文件时出错")
+        console.warn(error)
+      },
+      complete: function () {
+
+      }
+    })
+  },
+  error(e) {
+    console.log(e.detail)
+  }
+})
